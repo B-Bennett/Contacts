d7e99d13817eff6b6346037f51ab85c9838ba46e
BennettIronYard <bennettiy12@gmail.com>
2015-10-13 10:21:51 -0400

    Arry with HashMap. add test code for constructors

---
+++ src/Contact.java
@@ -0,0 +1,14 @@
+/**
+ * Created by BennettIronYard on 10/13/15.
+ */
+public class Contact {
+    public String name;
+    public String address;
+    public String email;
+    public int age;
+
+    public Contact(String name, int age) {
+        this.name = name;
+        this.age = age;
+    } // public Contact
+} // public class

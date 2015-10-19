b62217d0b8c2c14124e74d7dc540a304f10b8657
BennettIronYard <bennettiy12@gmail.com>
2015-10-19 11:16:00 -0400

    two ways of getting randoms

--- src/Exercsie3.java
+++ src/Exercsie3.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Created by BennettIronYard on 10/19/15.
@@ -10,11 +11,28 @@ public class Exercsie3 {
         HashMap<String, ArrayList<Contact>> people = new HashMap();
 
         for (String name : names) { //will print out the first for names.
-            for (String name2 : names) { //loop back over
+            for (String name2 : names) { //inner loop back over
                 if (!name.equals(name2)) { // stop duplicates
-                    System.out.println(String.format("%s %s", name, name2));
+                    Contact c = new Contact();
+                    c.name = name2;
+                    ArrayList<Contact> list = people.get(name);   //
+                    if (list == null) {
+                        list = new ArrayList();
+                        list.add(c);
+                        people.put(name, list);
+                    }else {
+                        list.add(c);
+                    }
                 }//end of if statement
             }//end of 2nd for loop
         }//end of for loop
+
+        double randNum = Math.random();
+        int index = (int) (randNum * 4);
+        System.out.println(names[index]);
+
+        Random rand = new Random();
+        index = rand.nextInt(4);
+        System.out.println(names[index]);
     }//end of public static void
 }//end of public class
